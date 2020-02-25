c8b25fe7a944e560452ea80a98b59fbd96a49b99
Rashed <rashedulislam8741@yahoo.com>
2020-02-25 23:23:24 +0600

    First docker application to demonstrate how docker works!

---
+++ src/main/java/com/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	@RequestMapping("/")
+	public String showHome(final Model model) {
+		model.addAttribute("title", "Spring Boot with Docker");
+		model.addAttribute("message", "Welcome to Docker container!");
+		return "index";
+	}
+}
