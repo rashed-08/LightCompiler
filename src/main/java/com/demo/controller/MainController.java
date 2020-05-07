12d43695d6036efc39f09647096b7bb207ec3835
Rashed <rashedulislam8741@yahoo.com>
2020-05-07 16:43:42 +0600

    update rest api and fixed some minor bug

--- src/main/java/com/demo/controller/MainController.java
+++ src/main/java/com/demo/controller/MainController.java
@@ -1,40 +1,35 @@
 package com.demo.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.model.Solution;
 import com.demo.service.impl.ControllerServiceImpl;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin("*")
 public class MainController {
-    
+     
     @Autowired
     private ControllerServiceImpl controllerService;
-
-    @RequestMapping(value = {"/","/solution"})
-    public String showSubmitPage(Model model) {
-        model.addAttribute("solution", new Solution());
-        return "solution";
-    }
     
-    @RequestMapping(value = "/solution", method = RequestMethod.POST)
-    public String compile(@ModelAttribute final Solution solution, final BindingResult result){
-        ArrayList<String> outputList;
-        if (result.hasErrors()) {
-            return "solution";
-        }
+    @PostMapping("/submit")
+    public ResponseEntity<List<String>> compile(@RequestBody final Solution solution, final BindingResult result){
+        List<String> outputList = new ArrayList<>();
         int successfullyExited = controllerService.compile(solution);
         if (successfullyExited == 0) {
             outputList = controllerService.execute();
         }
-        return "solution";
+        return ResponseEntity.ok(outputList);
     }
 }
